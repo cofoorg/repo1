cf5a728e23c54c207f8df515b692d285181b971e
riteshbehal <49110318+riteshbehal@users.noreply.github.com>
2023-11-02 19:32:09 +0530

    Update CommonUtils.java

--- CommonUtils.java
+++ CommonUtils.java
@@ -2,6 +2,6 @@ Welcome to CommonUtils.java
 class GeeksforGeeks {
     public static void main(String[] args)
     {
-        System.out.println("I am a Geek");
+        System.out.println("I am a Geek person");
     }
 }
